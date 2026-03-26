ae9d495d66f5519a8612842ae58e22b184e6047a
shifengdongma <147428125+shifengdongma@users.noreply.github.com>
2026-03-26 13:19:06 +0800

    chore: auto-commit latest changes

--- frontend/src/views/meeting/MeetingRoomView.vue
+++ frontend/src/views/meeting/MeetingRoomView.vue
@@ -83,7 +83,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onBeforeUnmount, onMounted, reactive, ref } from 'vue'
+import { computed, onBeforeUnmount, onMounted, reactive, ref, watch } from 'vue'
 import { ElMessage } from 'element-plus'
 import { useRoute } from 'vue-router'
 
